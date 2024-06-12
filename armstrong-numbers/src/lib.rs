94acb6b7e9c571a6d0d00fe6484009fd1d6505ac
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-06-12 20:02:35 -0300

    CHANGED: Armstrong Numbers simplified form.

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,16 +1,14 @@
 pub fn is_armstrong_number(num: u64) -> bool {
-    let mut digits = Vec::new();
-    let mut sum:u64 = 0;
-    let mut aux_num = num;
-
-    while aux_num > 0 {
-        digits.push(aux_num % 10);
-        aux_num /= 10;
-    }
+    let digits: Vec<u64> = num
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect();
 
     let digits_num = digits.len() as u32;
-
-    digits.iter().for_each(|digit| sum += digit.pow(digits_num));
+    let sum: u64 = digits.iter()
+                         .map(|digit|digit.pow(digits_num))
+                         .sum();
 
     sum == num
 }
